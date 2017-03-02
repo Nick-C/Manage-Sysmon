8b7af7e68f19fb8e2d6ebc1b1f398bb59852c2de
Nick-C <nick@monkeydust.net>
2017-03-02 16:42:57 +0000

    Initial version of the Install-Sysmon function

--- Manage-Sysmon.ps1
+++ Manage-Sysmon.ps1
@@ -1,5 +1,6 @@
 Function Install-Sysmon {
-
+#requires -version 4.0
+#requires -runasadministrator
     <#
 
     .SYNOPSIS
@@ -33,22 +34,86 @@ Function Install-Sysmon {
     [CmdletBinding(DefaultParameterSetName = 'config')]
 
     Param(
-    [Parameter(Mandatory = $true, ParameterSetName = 'config')][string]$config,
-    [string]$sysmonlocation
+    [Parameter(Mandatory = $true, ParameterSetName = 'config')]
+    [ValidateScript({Test-Path $_ -PathType 'Leaf' })]
+    [string]
+    $config
+    ,
+    [ValidateScript({Test-Path $_ -PathType 'Leaf' })]
+    [string]
+    $sysmonlocation
+    )
+    
+    # Debug: dump the variables we got passed.
+    Write-Verbose "Sysmon path: $($sysmonlocation)"
+    Write-Verbose "Config path: $($config)"
+
+    # If a sysmon.exe location wasn't passed via a parameter, lets assume it exists in the current directory for now.
+   
+    Write-Verbose "test is: $test"
+    if(!(Test-Path variable:global:sysmonlocation)) {
+        Write-Verbose "No Sysmonlocation passed, detecting via current directory"
+        $currentpath = Get-AbsolutePath
+        $sysmonlocation = $currentpath + "\sysmon.exe"
+        Write-Verbose "Sysmonlocation now set to: $sysmonlocation"
+    }
+    # Now we definately have a path to sysmon.exe (either provided or assumed), lets try and verify sysmon.exe actually exists.
+    try {
+        Write-Verbose "Testing if we can find sysmon.exe at $sysmonlocation"
+        $null = Test-Path $sysmonlocation -Include 'sysmon.exe'
+    }
+    catch {
+        Write-Verbose "Contents of sysmonlocation are: $sysmonlocation"
+        Write-Error "Unable to find sysmon.exe, verify it is in the same folder as this script or the path is passed via -sysmonlocation"
+    }
+    
+
+    # We know $config is a valid file thanks to parameter validation, now lets quickly test if it is an xml file.
+    Write-Verbose "Testing if the given config file is xml"
+    if ((Test-Path $config -Include '*.xml') -eq $false) {
+        Write-Verbose "Contents of config are: $config"
+        Write-Error "Unable to verify that provided config is a valid xml file."
+     }
+
+    # This is only really needed for older powershell versions, if you are running this from PS 4.0 or newer you can remove the next two if statements as these checks are handled
+    # by the #requires statements at the start of the function. Likewise to allow this script to run on older PS versions, remove the #requires statements at the beginning of each function.
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        Write-Warning "You do not have Administrator rights to run this script! Please re-run this script as an Administrator!"
+        Break
+    }
+    if(-not (Get-Process -id $pid).MainWindowTitle -Like "Administrator:*") {
+        Write-Warning "Please run this script via an Administrative Powershell session!"
+        Break
+    }
+
+
+    # Before we attempt to install, lets test to see if sysmon might already be installed by checking the registry and Windows folder
+    if((Test-Path HKLM:\System\CurrentControlSet\Services\Sysmon) -or (Test-Path HKLM:\System\CurrentControlSet\Services\SysmonDrv) -or (Test-Path $env:windir\sysmon.exe -PathType 'Leaf')) {
+        Write-Warning "An existing Sysmon install has been detected, please uninstall fully before running again."
+        Break
+    }
+
+    # Install sysmon
+    $sysmoninstallcmd = "-i $config -accepteula"
+    Write-Verbose "Install command: $sysmonlocation $sysmoninstallcmd"
+    Start-Process $sysmonlocation -ArgumentList $sysmoninstallcmd -Wait -WindowStyle Hidden
+}
+
+Function Get-AbsolutePath {
+
+    [CmdletBinding()]
+    Param(
+        [parameter(
+            Mandatory=$false,
+            ValueFromPipeline=$true
+        )]
+        [String]$relativePath=".\"
     )
 
-    # TODO: Test sysmon path.
-
-
-    # TODO: Test config path.
-
-
-    # TODO: Check for admin ps session.
-
-
-    # TODO: Check for existing install via registry.
-
-
-    # TODO: Install sysmon
+    if (Test-Path -Path $relativePath) {
+        return (Get-Item -Path $relativePath).FullName -replace "\\$", ""
+    } else {
+        Write-Error -Message "'$relativePath' is not a valid path" -ErrorId 1 -ErrorAction Stop
+    }
 
 }
